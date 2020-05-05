742eb42a8fb2fa30cd60278eea315c49b81ff740
Noah Gilmore <noah.w.gilmore@gmail.com>
2020-05-05 10:45:38 -0700

    Add react-ga

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { StaticKitProvider } from '@statickit/react'
 import { useForm, ValidationError } from '@statickit/react';
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
+import ReactGA from "react-ga";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("contactForm");  
@@ -63,44 +64,51 @@ const InnerContainer = styled.div`
     }
 `;
 
-const IndexPage = () => (
-    <StaticKitProvider site="701e580f4480">
-        <Helmet>
-            <title>Fluency: Pro macOS app for Confluence Server</title>
-        </Helmet>
-        <div className="outerWrapper h-screen flex flex-col dark-mode:bg-gray-800 dark-mode:text-white"
-            style={{fontFamily: "'Lato', Helvetica, sans-serif"}}>
-            <Nav />
-            <div className="innerWrapper pl-8 pt-8 pb-8 md:p-32 md:pt-8 flex flex-row">
-                <InnerContainer className="pr-0 md:pr-8" style={{maxWidth: "100%"}}>
-                    <div className="pr-8 md:pr-0">
-                        <div className="flex flex-row items-center justify-start pb-4"> 
-                            <div style={{width: "40px", height: "40px"}}><Image filename="logo.png" /></div>
-                            <div className="text-2xl pl-4">Fluency</div>
+class IndexPage extends React.Component {
+    componentDidMount() {
+        ReactGA.initialize('UA-165585096-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+
+    render() {
+        return (<StaticKitProvider site="701e580f4480">
+            <Helmet>
+                <title>Fluency: Pro macOS app for Confluence Server</title>
+            </Helmet>
+            <div className="outerWrapper h-screen flex flex-col dark-mode:bg-gray-800 dark-mode:text-white"
+                style={{fontFamily: "'Lato', Helvetica, sans-serif"}}>
+                <Nav />
+                <div className="innerWrapper pl-8 pt-8 pb-8 md:p-32 md:pt-8 flex flex-row">
+                    <InnerContainer className="pr-0 md:pr-8" style={{maxWidth: "100%"}}>
+                        <div className="pr-8 md:pr-0">
+                            <div className="flex flex-row items-center justify-start pb-4"> 
+                                <div style={{width: "40px", height: "40px"}}><Image filename="logo.png" /></div>
+                                <div className="text-2xl pl-4">Fluency</div>
+                            </div>
+                            <h1 className="font-black text-5xl pb-5">Pro macOS app for Confluence Server</h1>
+                            <ul className="text-lg pl-8 pb-8" style={{listStyleType: "disc"}}>
+                                <li className="p-1">Fast, local search for only the spaces you need</li>
+                                <li className="p-1">Create and publish pages with real Markdown</li>
+                                <li className="p-1">View local pages without signing in</li>
+                                <li className="p-1">Copy links to headings in one click</li>
+                            </ul>
+                            <ContactForm />
                         </div>
-                        <h1 className="font-black text-5xl pb-5">Pro macOS app for Confluence Server</h1>
-                        <ul className="text-lg pl-8 pb-8" style={{listStyleType: "disc"}}>
-                            <li className="p-1">Fast, local search for only the spaces you need</li>
-                            <li className="p-1">Create and publish pages with real Markdown</li>
-                            <li className="p-1">View local pages without signing in</li>
-                            <li className="p-1">Copy links to headings in one click</li>
-                        </ul>
-                        <ContactForm />
-                    </div>
-                    <div className="block md:hidden pt-8" style={{maxWidth: "100%", overflow: "hidden"}}>
-                        <div style={{position: "relative", width: "170%", overflow: "hidden"}}>
+                        <div className="block md:hidden pt-8" style={{maxWidth: "100%", overflow: "hidden"}}>
+                            <div style={{position: "relative", width: "170%", overflow: "hidden"}}>
+                                <Image filename="screenshot.png" />
+                            </div>
+                        </div>
+                    </InnerContainer>
+                    <div className="hidden md:block">
+                        <div style={{width: "1000px"}}>
                             <Image filename="screenshot.png" />
                         </div>
                     </div>
-                </InnerContainer>
-                <div className="hidden md:block">
-                    <div style={{width: "1000px"}}>
-                        <Image filename="screenshot.png" />
-                    </div>
                 </div>
             </div>
-        </div>
-    </StaticKitProvider>
-)
+        </StaticKitProvider>);
+    }
+}
 
 export default IndexPage
